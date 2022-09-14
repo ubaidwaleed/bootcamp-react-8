df61fb3cb7700a434ea9f910f58151a7f2ab4ab1
ubaid-waleed-epesys <106673699+ubaidwaleed@users.noreply.github.com>
2022-09-14 16:30:58 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+console.log("fetch api ");
+async function fetchAPI() {
+  const my_api = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  console.log(my_api, "API");
+  const json = await my_api.json();
+  console.log(json, "json");
+}
+
+async function postAPI() {
+  const api = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify({
+      title: "bootcamp",
+      body: "js",
+      userId: 1,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  });
+}
